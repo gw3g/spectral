411dcf80140fa81c2d0a32ae17e2c3f9a5596c4c
Greg Jackson <jackson@itp.unibe.ch>
2019-03-07 17:04:38 +0100

    first files

---
+++ core.h
@@ -0,0 +1,35 @@
+#define sgn(x) (double) ((x>0)-(x<0))
+#define lga(x) log(fabs(x))
+#define clp(x,a,b) max(a,min(b,x))
+
+double  f(double,int),
+       fb(double,int);
+
+/*--------------------------------------------------------------------*/
+
+#define kp ((k0+k)*.5)
+#define km ((k0-k)*.5)
+#define K2 (k0*k0-k*k)
+extern double k0, k; // external 4-momentum
+
+struct master {
+  int s[6]; // statistical configuration
+  int m, n; // p^n.q^m
+  master(int _m, int _n, int _s[3])
+    : m(_m), n(_n) {
+        // provided:
+        s[0] = _s[0]; s[1] = _s[1]; s[2] = _s[2];
+        // derived:
+        s[3] = _s[0]*_s[1]*_s[2];
+        s[4] = _s[0]*_s[1];
+        s[5] = _s[0]*_s[2];
+    }
+  double operator ()(double _k0, double _k) const { 
+    k0=_k0; k=_k; // reset K
+    return K2;
+  };
+  //virtual double rho() = 0;
+};
+
+/*--------------------------------------------------------------------*/
+
---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include "core.h"
+#include <iostream>
+double k0, k;
+
+using namespace std;
+
+int main() {
+  master *Rho;
+  int s[] = {1,1,-1};
+  Rho = new rho11100(0,2,s);
+  cout << "m=" << Rho->m << endl;
+  cout << "n=" << Rho->m << endl;
+  cout << "s0=" << Rho->s[0] << 
+      " ,  s1=" << Rho->s[1] <<
+      " ,  s2=" << Rho->s[2] <<
+      " ,  s3=" << Rho->s[3] <<
+      " ,  s4=" << Rho->s[4] <<
+      " ,  s5=" << Rho->s[5] << endl << endl;
+
+  cout << (*Rho)(2.,0.) << endl;
+  cout << (*Rho)(2.,1.) << endl;
+  cout << (*Rho)(2.,2.) << endl;
+  cout << k0 << ", " << k << endl;
+}
