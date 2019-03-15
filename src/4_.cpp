40f12aa7f2eb72cde8b8954864356bf5fe2b600f
Greg Jackson <jackson@itp.unibe.ch>
2019-03-15 16:21:14 +0100

    Many difficulties with 4.

--- src/4_.cpp
+++ src/4_.cpp
@@ -12,11 +12,47 @@ double W_iv(double p, double q) {
 }
 
 /*--------------------------------------------------------------------*/
+struct region {
+  Master *R;
+  int _s[3]={(R->s)[0],(R->s)[1],(R->s)[2]};
+  int _m = R->m;
+  int _n = R->n;
+  virtual double func(double,double) = 0;
+  double operator ()(double x,double y) {return func(x,y);};
+};
+
+template<typename F>
+Finite<F> make(F f,double a, double b) {
+  return Finite<F>(f,a,b);
+}
+
+template<typename F>
+SemiInf<F> make(F f,double a) {
+  return SemiInf<F>(f,a);
+}
+/*
+struct above_LC_1A : region {
+  double func(double x, double y) {
+  return make([&](double p, double pd) { 
+  return make([&](double q, double qd) {
+      double pqr[3] = {p,q,k0-p-q};
+      return W_iv(p,q)*fff(pqr,_m,_n,_s);
+  },//  q_min,  q_max
+        kp-p,   p     )(y);
+  },//  p_min,  p_max
+        .5*kp,  km    )(x);
+  };
+};*/
 
 struct rho11100 : Master {
   double eval();
-  static double above_LC_1A(double,double);
+  double above_LC_1A(double,double);
+  //double above_LC_2A(double,double);
+
   rho11100(int _m, int _n, int _s[3]) : Master(_m,_n,_s) {
+    //reg = new above_LC_1A();
+    //(*reg).R = this;
+    //above_LC_1A.R = this;
   }
 };
 // function for MAIN
@@ -31,26 +67,25 @@ double rho11100::eval() {
   //int cut[3] = {s[0],s[1],s[2]};
   //res += fff(pqr,m,n,cut);
   //return -res*.25*pow(OOFP,3);
-  res += integrate_2d(above_LC_1A);
+  //res += integrate_2d(i);
+  //prt = above_LC_1A;
+  //res += integrate_2d(above_LC_1A);
+  //res += above_LC_1A(.4,.3);
+  //res += above_LC_2A(.2,.04);
   //res += above_LC_1A(.4,.3);
   return res;
 }
 
-template<typename F>
-Finite<F> make(F f,double a, double b) {
-  return Finite<F>(f,a,b);
-}
 
 double rho11100::above_LC_1A(double x, double y) 
 {
-  //int _s[3]={(this->s)[0],(this->s)[1],(this->s)[2]};
-  int _s[3]={1,1,1};
-  //int _m = this->m;
-  //int _n = this->n;
+  int _s[3]={(this->s)[0],(this->s)[1],(this->s)[2]};
+  int _m = this->m;
+  int _n = this->n;
   return make([&](double p, double pd) { 
   return make([&](double q, double qd) {
       double pqr[3] = {p,q,k0-p-q};
-      return W_iv(p,q)*fff(pqr,0,0,_s);
+      return W_iv(p,q)*fff(pqr,_m,_n,_s);
   },//  q_min,  q_max
         kp-p,   p     )(y);
   },//  p_min,  p_max
